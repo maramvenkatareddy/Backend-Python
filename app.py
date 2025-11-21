0e7f918a43f615942178414a587583a913a64f14
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2025-11-21 12:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def delete_user(id):
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
